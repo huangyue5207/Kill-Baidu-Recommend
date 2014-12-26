dd8eabd1ee3e3b0d174a7a6f510ac93d8ff040e9
huangyue <huangyue@yw-0-0.(none)>
2014-12-26 18:27:38 +0800

    Kill-Baidu-Recommend

---
+++ kill_baidu_recommend.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * The purpose of this function call is going to hide 
+ * recommend section of Baidu which loaded the class
+ * selector 'cr-offset'.
+**/
+
+$("html").append("<style> .cr-offset{display:none;} </style>");
+
